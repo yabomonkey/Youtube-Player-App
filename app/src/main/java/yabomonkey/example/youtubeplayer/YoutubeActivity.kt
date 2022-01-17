c75ccda8b23a739f086fc4a0ccd9965d4c3df933
Dan <yabomonkey@gmail.com>
2022-01-16 23:33:54 -0800

    Created widget and matched constraints to parents

--- app/src/main/java/yabomonkey/example/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/yabomonkey/example/youtubeplayer/YoutubeActivity.kt
@@ -1,15 +1,32 @@
 package yabomonkey.example.youtubeplayer
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+//        setContentView(R.layout.activity_youtube)
+//        val layout = findViewById<ConstrantLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600,180)
+//        button1.text = "Button Added"
+//        layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(
