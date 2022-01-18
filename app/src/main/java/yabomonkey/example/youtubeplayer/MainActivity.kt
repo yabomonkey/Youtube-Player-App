27dcf76ebb99a0ff991a22eb9e2e6ce7cc4ce6dc
Dan <yabomonkey@gmail.com>
2022-01-18 01:00:44 -0800

    Added listeners and code to buttons
    In Activity_main.xml

--- app/src/main/java/yabomonkey/example/youtubeplayer/MainActivity.kt
+++ app/src/main/java/yabomonkey/example/youtubeplayer/MainActivity.kt
@@ -1,11 +1,35 @@
 package yabomonkey.example.youtubeplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import yabomonkey.example.youtubeplayer.databinding.ActivityMainBinding
+import java.lang.IllegalArgumentException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnPlaySingle.setOnClickListener(this)
+        binding.btnStandalone.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        val intent = when (view.id) {
+            R.id.btnPlaySingle -> Intent(this, YoutubeActivity::class.java)
+            R.id.btnStandalone -> Intent(this, StandaloneActivity::class.java)
+            else -> throw IllegalArgumentException("Undefined button click")
+        }
+        startActivity(intent)
+    }
+
+
 }
